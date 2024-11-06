f980cd759065c83627d22af62e896083b49496e2
bots4MDSA@GH <MDSArobotics@gmail.com>
2024-11-06 16:49:16 -0500

    has arm braking

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Linear.java
@@ -69,6 +69,10 @@ public class BasicOpMode_Linear extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+//        int armSweep = 2.049; //120 degrees = 2.049 radians
+//        int armMin = arm.getCurrentPosition();
+//        int armMax = armMin + armSweep;
+
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
@@ -82,6 +86,7 @@ public class BasicOpMode_Linear extends LinearOpMode {
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
         arm.setDirection(DcMotor.Direction.REVERSE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Wait for the game to start (driver presses START)
         waitForStart();
@@ -118,83 +123,13 @@ public class BasicOpMode_Linear extends LinearOpMode {
             rightDrive.setPower(rightPower);
             arm.setPower(armPower);
 
-            // Show the elapsed game time and wheel power.
+            // Show the elapsed game time and wheel power
+            int cp = arm.getCurrentPosition();
+
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Arm", "arm (%d)", cp);
             telemetry.update();
         }
     }
-
-//private class ConceptExternalHardwareClass extends LinearOpMode {
-//
-//    // Create a RobotHardware object to be used to access robot hardware.
-//    // Prefix any hardware functions with "robot." to access this class.
-//    RobotHardware   robot       = new RobotHardware(this);
-//
-//    @Override
-//    public void runOpMode() {
-//        double drive        = 0;
-//        double turn         = 0;
-//        double arm          = 0;
-//        double handOffset   = 0;
-//
-//        // initialize all the hardware, using the hardware class. See how clean and simple this is?
-//        robot.init();
-//
-//        // Send telemetry message to signify robot waiting;
-//        // Wait for the game to start (driver presses PLAY)
-//        waitForStart();
-//
-//        // run until the end of the match (driver presses STOP)
-//        while (opModeIsActive()) {
-//
-//            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
-//            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
-//            // This way it's also easy to just drive straight, or just turn.
-//            drive = -gamepad1.left_stick_y;
-//            turn  =  gamepad1.right_stick_x;
-//
-//            // Combine drive and turn for blended motion. Use RobotHardware class
-//            robot.driveRobot(drive, turn);
-//
-//            // Use gamepad left & right Bumpers to open and close the claw
-//            // Use the SERVO constants defined in RobotHardware class.
-//            // Each time around the loop, the servos will move by a small amount.
-//            // Limit the total offset to half of the full travel range
-//            if (gamepad1.right_bumper)
-//                handOffset += robot.HAND_SPEED;
-//            else if (gamepad1.left_bumper)
-//                handOffset -= robot.HAND_SPEED;
-//            handOffset = Range.clip(handOffset, -0.5, 0.5);
-//
-//            // Move both servos to new position.  Use RobotHardware class
-//            robot.setHandPositions(handOffset);
-//
-//            // Use gamepad buttons to move arm up (Y) and down (A)
-//            // Use the MOTOR constants defined in RobotHardware class.
-//            if (gamepad1.y)
-//                arm = robot.ARM_UP_POWER;
-//            else if (gamepad1.a)
-//                arm = robot.ARM_DOWN_POWER;
-//            else
-//                arm = 0;
-//
-//            robot.setArmPower(arm);
-//
-//            // Send telemetry messages to explain controls and show robot status
-//            telemetry.addData("Drive", "Left Stick");
-//            telemetry.addData("Turn", "Right Stick");
-//            telemetry.addData("Arm Up/Down", "Y & A Buttons");
-//            telemetry.addData("Hand Open/Closed", "Left and Right Bumpers");
-//            telemetry.addData("-", "-------");
-//
-//            telemetry.addData("Drive Power", "%.2f", drive);
-//            telemetry.addData("Turn Power",  "%.2f", turn);
-//            telemetry.addData("Arm Power",  "%.2f", arm);
-//            telemetry.addData("Hand Position",  "Offset = %.2f", handOffset);
-//            telemetry.update();
-//
-//            // Pace this loop so hands move at a reasonable speed.
-//            sleep(50);
-//        }
-    }
+}
