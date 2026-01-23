e236279d246ba3e2561f67f2123e2cd19b620f36
smweaver57 <smweave@gmail.com>
2026-01-23 16:05:53 -0500

    Added gamepad instructions to StarterBotTeleop telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Y2526/starterbot/StarterBotTeleop_original.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Y2526/starterbot/StarterBotTeleop_original.java
@@ -30,7 +30,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.Y2526.starterbot
+package org.firstinspires.ftc.teamcode.Y2526.starterbot;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 
