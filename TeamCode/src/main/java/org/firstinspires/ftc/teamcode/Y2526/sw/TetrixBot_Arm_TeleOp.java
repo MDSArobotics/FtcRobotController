7876c229aced50cf35d651f457739c3df22aba2e
smweaver57 <smweave@gmail.com>
2026-05-28 10:07:25 -0400

    Added TetrixBot_Arm_TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Y2526/sw/TetrixBot_Arm_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Y2526/sw/TetrixBot_Arm_TeleOp.java
@@ -13,16 +13,16 @@
 
 package org.firstinspires.ftc.teamcode.Y2526.sw;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "TetrixBot_Arm_TeleOp")
-@Disabled
+//@Disabled
 public class TetrixBot_Arm_TeleOp extends LinearOpMode {
 
     // Motors
@@ -31,67 +31,37 @@ public class TetrixBot_Arm_TeleOp extends LinearOpMode {
     public DcMotor  rightDrive  = null; //the right drivetrain motor
     public DcMotor  armMotor    = null; //the arm motor
     public Servo    wrist       = null; //the wrist servo
-    public Servo    gripper     = null; // gripper servo
+    public Servo    claw        = null; //the claw servo
 
-    // Sensors
-    /** The colorSensor field will contain a reference to our color sensor hardware object */
-    //public NormalizedColorSensor colorSensorLeft = null;
-    //public NormalizedColorSensor colorSensorRight = null;
+    public double DRIVE_SPEED = 0.8;
+    public double TURN_SPEED;
+    final double SPEED_STEP = 0.2;
+    public double ARM_SPEED = 0.4;
 
+    // wrist positions.
+    final double UP   = 0.6;
+    final double DOWN = 0.35;
+    final double PARKED = 0.95;
 
-    /* The COUNTS_PER_INCH constant is the number of encoder counts for each of inch of travel by the robot.
-    The motors we use for this robot are TETRIX MAX TorqueNADOs which have a 60:1 gear ratio,
-    delivering roughly 100 RPM and 700 oz-in. of stall torque at 12 volts.
-    The motor encoder outputs 1440 pulses or counts per revolution of the motor shaft.
-    The motors are driving the wheels directly with no external gearing.
-     */
-
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;   // TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0;     // No external drive gearing
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;    // For calculating circumference
-    static final double     COUNTS_PER_INCH  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-
-    /* The ARM_COUNTS_PER_DEGREE constant is the number of encoder counts for each degree of arm rotation.
-    This value reflects both the internal gearing of the motor and the external gearing driving the arm.
-     */
-    static final double     ARM_GEAR_REDUCTION    = 1.5;     // 120T spur driven by 80T pinion gear.
-    final double ARM_COUNTS_PER_DEGREE =  COUNTS_PER_MOTOR_REV * ARM_GEAR_REDUCTION  * 1/360.0;
-    static final double     ARM_SPEED              = 0.5;
-
-
-    /* These constants hold the position that the arm is commanded to run to.
-    These are relative to where the arm was located when you start the OpMode. So make sure the
-    arm is reset to collapsed inside the robot before you start the program.
-
-    In these variables you'll see a number in degrees, multiplied by the ticks per degree of the arm.
-    This results in the number of encoder ticks the arm needs to move in order to achieve the ideal
-    set position of the arm.  */
-
-    final double ARM_HOME  = 0.0;   // ARM starting position, about 6" above battery
-    final double ARM_DOWN  = 10 * ARM_COUNTS_PER_DEGREE;  // About 1" above battery
-    final double ARM_LEVEL = 80 * ARM_COUNTS_PER_DEGREE;  // Level with ground
-
-
-    /* Variables to store the wrist and gripper positions. */
-    final double WRIST_STRAIGHT   = 1.0;
-    final double WRIST_BENT = 0.5;
-    final double GRIPPER_CLOSED = 0;
-    final double GRIPPER_OPEN = 90;
-
-    /* A number in degrees that the triggers can adjust the arm position by */
-    final double FUDGE_FACTOR = 15 * ARM_COUNTS_PER_DEGREE;
-
-    /* Variables that are used to set the arm to a specific position */
-    double armPosition = ARM_HOME;
-    double armPositionFudgeFactor;
+    // Claw gripper positions.
+    final double CLOSED = 0.20; // 18 degrees
+    final double OPEN = 0.65;  // 120 degrees
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("*  Left Joystick Up/Down: ", "Drive Forward/Backward");
+        telemetry.addData("*  Right Joystick Left/Right ", "Turn Left/Right");
+        telemetry.addData("*  Bumper Left/Right ", "Drive Speed Down/Up");
+        telemetry.addData("*  Trigger Left/Right ", "Arm Position Down/Up");
+        telemetry.addData("*  DPAD Left/Right", "Claw Closed/Open");
+        telemetry.addData("*  DPAD Down/Up", "Wrist Down/Up");
+        telemetry.addData("Status", "Run Time: " + runtime);
         telemetry.update();
+
+        // Initialize gamepad states.  Setting these values to new Gamepad() will default all
+        // boolean values as false and all float values as 0
+        Gamepad currentGamepad1 = new Gamepad();
+        Gamepad previousGamepad1 = new Gamepad();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration step
@@ -104,10 +74,10 @@ public class TetrixBot_Arm_TeleOp extends LinearOpMode {
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        armMotor.setDirection(DcMotor.Direction.FORWARD);
+        // The settings here assume a 3:1 gear ratio for the drive motors requiring direction flips
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
 
         /* Setting zeroPowerBehavior to BRAKE enables a "brake mode". This causes the motor to slow down
         much faster when it is coasting. This creates a much more controllable drivetrain. As the robot
@@ -117,28 +87,13 @@ public class TetrixBot_Arm_TeleOp extends LinearOpMode {
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        /* Before starting the armMotor. We'll make sure the TargetPosition is set to 0.
-        Then we'll set the RunMode to RUN_TO_POSITION. And we'll ask it to stop and reset encoder.
-        If you do not have the encoder plugged into this motor, it will not run in this code. */
-        armMotor.setTargetPosition(0);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
         /* Define and initialize servos.*/
         wrist  = hardwareMap.get(Servo.class, "wrist");
-        gripper  = hardwareMap.get(Servo.class, "gripper");
+        claw  = hardwareMap.get(Servo.class, "claw");
 
-        /* The wrist is straight and the gripper is closed.*/
-        wrist.setPosition(WRIST_STRAIGHT);
-        gripper.setPosition(GRIPPER_CLOSED);
-
-        // Get a references to  sensor objects.
-        // It's recommended to use NormalizedColorSensor over ColorSensor, because NormalizedColorSensor
-        // consistently gives values between 0 and 1, while the values you get from ColorSensor are
-        // dependent on the specific sensor you're using.
-        //colorSensorLeft = hardwareMap.get(NormalizedColorSensor.class, "sensor_color_left");
-        //colorSensorRight = hardwareMap.get(NormalizedColorSensor.class, "sensor_color_right");
-        // To do: Add functionality to the color sensors
+        /* The wrist is parked and the claw is closed.*/
+        wrist.setPosition(PARKED);
+        claw.setPosition(CLOSED);
 
         // Wait for the game to start (driver presses START)
         waitForStart();
@@ -151,11 +106,22 @@ public class TetrixBot_Arm_TeleOp extends LinearOpMode {
             double leftPower;
             double rightPower;
 
+            // Store the gamepad values from the previous loop and current loop
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+
+            if (currentGamepad1.left_bumper && !previousGamepad1.left_bumper) {
+                DRIVE_SPEED = Range.clip( DRIVE_SPEED - SPEED_STEP, 0.4, 1.0);
+            } else if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {
+                DRIVE_SPEED = Range.clip( DRIVE_SPEED + SPEED_STEP, 0.4, 1.0);
+            }
+            TURN_SPEED = DRIVE_SPEED - 0.2;
+
             // Drive the bot in "Point of View" POV Mode versus tank drive
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
+            double drive = -gamepad1.left_stick_y * DRIVE_SPEED;
+            double turn  =  gamepad1.right_stick_x * TURN_SPEED;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
@@ -163,29 +129,39 @@ public class TetrixBot_Arm_TeleOp extends LinearOpMode {
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            // Press left trigger to run the CR servos to move the artifact towards the flywheel
-            // Press the right bumper to move the gate servo forward to push artifact into flywheel and launch
-            // Press the left bumper to move the gate servo back to original position
-
-            if(gamepad1.right_bumper){
-                moveArm(ARM_DOWN);
+            if (gamepad1.dpad_up){
+                wrist.setPosition(UP);
             }
-            else if (gamepad1.left_bumper){
-                moveArm(ARM_LEVEL);
+            else if (gamepad1.dpad_down){
+                wrist.setPosition(DOWN);
+            }
+            if (gamepad1.dpad_left){
+                claw.setPosition(CLOSED);
+            }
+            else if (gamepad1.dpad_right){
+                claw.setPosition(OPEN);
+            }
+
+            if (gamepad1.right_trigger > 0.1) {
+                armMotor.setPower(ARM_SPEED); // Arm speed forward
+            } else if (gamepad1.left_trigger > 0.1) {
+                armMotor.setPower(-ARM_SPEED); // Arm speed reverse
+            } else {
+                armMotor.setPower(0.0); // Stop
             }
 
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("*  Left Joystick Up/Down: ", "Drive Forward/Backward");
+            telemetry.addData("*  Right Joystick Left/Right ", "Turn Left/Right");
+            telemetry.addData("*  Bumper Left/Right ", "Drive Speed Down/Up");
+            telemetry.addData("*  Trigger Left/Right ", "Arm Position Down/Up");
+            telemetry.addData("*  DPAD Left/Right", "Claw Closed/Open");
+            telemetry.addData("*  DPAD Down/Up", "Wrist Down/Up");
+            telemetry.addData("Status", "Run Time: " + runtime);
             telemetry.addData("Drive Motors Power", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            telemetry.addData("Motor Speed", "drive (%.2f), turn (%.2f), arm (%.2f)", DRIVE_SPEED, TURN_SPEED, ARM_SPEED);
             telemetry.update();
         }
-    }
-    public void  moveArm(double position){
-        double armPower = 0.5;
-        armMotor.setTargetPosition((int) position);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setPower(armPower);
     }
 }
 
